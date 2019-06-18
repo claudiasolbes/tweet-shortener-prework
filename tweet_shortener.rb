7323d92b4f00f81c05562b2b608644985403f6c3
Claudia Solbes <cssolbes@hotmail.com>
2019-06-18 14:58:35 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+  def word_substituer(tweet)
+  
+  dictionary = { "hello" => "hi",  "to" => "2",  "two" => "2",  "too" => "2",  "for" => "4",  "four" => "4",  "be" => "b",  "you" => "u",  "at" => "@",  "and" => "&" }
+    
+    tweet_array = tweet.split
+      tweet_array.each do |words|
+      if words.include? dictionary.keys
+        tweet_array.each do |key, value|
+        tweet.gsub!(key, value)
+        tweet_array.join
+       end
+      end
+    end
+  end
